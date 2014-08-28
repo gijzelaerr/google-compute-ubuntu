cc275fc5ef463270397edc9087dbc52c8d5c8cd3
Gijs Molenaar <gijs@pythonic.nl>
2014-08-28 17:33:40 +0200

    almost working

--- provision.sh
+++ provision.sh
@@ -13,7 +13,7 @@ sudo -E apt-get update -q
 sudo -E apt-get upgrade -y -q -u
 sudo -E apt-get install -y -q kpartx ethtool
 
-cd /vagrant
+cd /vagrant/download
 wget -c https://github.com/GoogleCloudPlatform/compute-image-packages/releases/download/${COMPUTE_VERSION}/python-gcimagebundle_${COMPUTE_VERSION}-1_all.deb
 wget -c https://github.com/GoogleCloudPlatform/compute-image-packages/releases/download/${COMPUTE_VERSION}/google-startup-scripts_${COMPUTE_VERSION}-1_all.deb
 wget -c https://github.com/GoogleCloudPlatform/compute-image-packages/releases/download/${COMPUTE_VERSION}/google-compute-daemon_${COMPUTE_VERSION}-1_all.deb
@@ -36,13 +36,20 @@ sudo update-grub2
 sudo su -c "echo "GOOGLE" > /etc/ssh/sshd_not_to_be_run"
 
 # download google cloud SDK
-wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
-tar zxvf google-cloud-sdk.tar.gz
-/vagrant/google-cloud-sdk/install.sh --usage-reporting false --disable-installation-options  --bash-completion true  --path-update true
-source ~/.bashrc
+cd /vagrant/download
+wget -c https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
+cd /opt
+tar zxvf /vagrant/download/google-cloud-sdk.tar.gz
+sudo /opt/google-cloud-sdk/install.sh --usage-reporting false --disable-installation-options  --bash-completion true  --path-update true
+source /opt/google-cloud-sdk/path.bash.inc
 
 # WARNING: after this you can't access the VM anymore
-sudo rm -rf /etc/ssh/ssh_host_*
+#sudo rm -rf /etc/ssh/ssh_host_*
 
 # make the image
-sudo gcimagebundle -d /dev/sda -r / -o /vagrant/output --loglevel=DEBUG --log_file=/tmp/image_bundle.log
+#sudo mkdir -p /image
+#sudo gcimagebundle -d /dev/sda -r / -o /image
+#sudo mv /image/* /vagrant/output
+
+
+# done
